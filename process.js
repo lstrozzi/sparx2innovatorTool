47dafa7ef63bb054517bbe92d26cc355a9cfa4b2
luca <strozzi.luca@gmail.com>
2024-03-28 14:58:12 +0100

    wunderbar

--- process.js
+++ process.js
@@ -1,6 +1,6 @@
-elements = {}
-connectors = {}
-diagrams = {}
+let allElements = {}
+let allConnectors = {}
+let localidmap = {}
 
 function extractTaggedValue(xmlElement, tag) {
     // extract information from the tagged values
@@ -61,8 +61,9 @@ function extractElements(xmlDoc, tag, tvmap) {
             }
 
             // add the information to the classes dictionary
-            let id = item['localid']
+            let id = item['id']
             elements[id] = item
+            localidmap['E-' + item['localid']] = item
         }
     }
 
@@ -75,29 +76,89 @@ function extractConnectors(xmlDoc, tag, tvmap) {
 
     for (var i = 0; i < xmlelements.length; i++) {
         var xmlelement = xmlelements[i];
-        if (xmlelement.getAttribute('isRoot') == 'false') {
-            // extract basic information
-            let item = {}
-            item['name'] = xmlelement.getAttribute('name')
-            item['id'] = xmlelement.getAttribute('xmi.id')
 
-            // tvmap is a dictionary of tagged values to extract. Key=tag, value:name of the key to be added to the item
-            for (var tag in tvmap) {
-                item[tvmap[tag]] = extractTaggedValue(xmlelement, tag);
-            }
+        // extract basic information
+        let item = {}
+        item['name'] = xmlelement.getAttribute('name')
+        item['id'] = xmlelement.getAttribute('xmi.id')
 
-            // add the information to the classes dictionary
-            let id = item['localid']
-            connectors[id] = item
+        // tvmap is a dictionary of tagged values to extract. Key=tag, value:name of the key to be added to the item
+        for (var tag in tvmap) {
+            item[tvmap[tag]] = extractTaggedValue(xmlelement, tag);
         }
+
+        // add the information to the classes dictionary
+        let id = item['id']
+        connectors[id] = item
+        localidmap['C-' + item['localid']] = item
     }
 
     return connectors
 }
 
-function processContents(xmlDoc) {
-    let allElements = {}
+function extractDiagramElements(xmldiagram, tag) {
+    var xmldiagramElements = xmldiagram.getElementsByTagName(tag)
+    var diagramElements = {}
 
+    for (var i = 0; i < xmldiagramElements.length; i++) {
+        var xmldiagramElement = xmldiagramElements[i];
+
+        if (xmldiagramElement.getAttribute('subject') != null) {
+            // extract basic information
+            let item = {}
+            item['geometry'] = xmldiagramElement.getAttribute('geometry')
+            item['subject'] = xmldiagramElement.getAttribute('subject')
+            item['seqno'] = xmldiagramElement.getAttribute('seqno')
+
+            // determine if it's a connector reference or an element reference
+            if (allElements[item['subject']] != null) {
+                item['type'] = 'element'
+            } else if (allConnectors[item['subject']] != null) {
+                item['type'] = 'connector'
+            } else {
+                item['type'] = 'unknown'
+            }
+
+            // add the information to the classes dictionary
+            let id = item['subject']
+            diagramElements[id] = item
+        }
+    }
+
+    return diagramElements
+}
+
+function extractDiagrams(xmlDoc, tag, tvmap) {
+    var xmldiagrams = xmlDoc.getElementsByTagName(tag)
+    var diagrams = {}
+
+    for (var i = 0; i < xmldiagrams.length; i++) {
+        var xmldiagram = xmldiagrams[i];
+        // extract basic information
+        let item = {}
+        item['name'] = xmldiagram.getAttribute('name')
+        item['id'] = xmldiagram.getAttribute('xmi.id')
+        item['type'] = xmldiagram.getAttribute('diagramType')
+        item['owner'] = xmldiagram.getAttribute('owner')
+
+        // tvmap is a dictionary of tagged values to extract. Key=tag, value:name of the key to be added to the item
+        for (var tag in tvmap) {
+            item[tvmap[tag]] = extractTaggedValue(xmldiagram, tag);
+        }
+
+        // extract the diagram elements and connectors
+        de = extractDiagramElements(xmldiagram, 'UML:DiagramElement')
+        item['diagramelements'] = de
+
+        // add the information to the classes dictionary
+        let id = item['id']
+        diagrams[id] = item
+    }
+
+    return diagrams
+}
+
+function processContents(xmlDoc) {
     var packages = extractElements(xmlDoc, 'UML:Package', {'parent': 'parentid'})
     console.log("Imported " + Object.keys(packages).length + " packages")
     for (var key in packages) {
@@ -128,7 +189,23 @@ function processContents(xmlDoc) {
     associations = extractConnectors(xmlDoc, 'UML:Association', {'ea_localid': 'localid', 'ea_stype': 'type', 'direction': 'direction', 'ea_sourceID': 'startid', 'ea_targetID': 'endid', 'ea_sourceType': 'starttype', 'ea_targetType': 'endtype', 'ea_sourceName': 'startname', 'ea_targetName': 'endname'})
     console.log("Imported " + Object.keys(associations).length + " associations")
     for (var key in associations) {
-        console.log(" > " + allElements[associations[key]['startid']]['name'] + " -> " + allElements[associations[key]['endid']]['name'])
+        console.log(" > " + localidmap['E-'+associations[key]['startid']]['name'] + " -> " + localidmap['E-'+associations[key]['endid']]['name'])
+        allConnectors[key] = associations[key]
+    }
+
+    diagrams = extractDiagrams(xmlDoc, 'UML:Diagram', {'ea_localid': 'localid', 'type': 'type2' })
+    console.log("Imported " + Object.keys(diagrams).length + " diagrams")
+    for (var key in diagrams) {
+        console.log(" > " + diagrams[key]['name'])
+        for (var key2 in diagrams[key]['diagramelements']) {
+            if (diagrams[key]['diagramelements'][key2]['type'] == 'element') {
+                console.log("   > " + allElements[diagrams[key]['diagramelements'][key2]['subject']]['name'])
+            } else if (diagrams[key]['diagramelements'][key2]['type'] == 'connector') {
+                console.log("   > " + allConnectors[diagrams[key]['diagramelements'][key2]['subject']]['name'])
+            } else {
+                console.log("   > unknown")
+            }
+        }
     }
 }
 
