8c3632ea78c7395931ed1a8c7c16241163dda9f8
luca <strozzi.luca@gmail.com>
2024-04-02 11:24:40 +0200

    cosmetic in the pretty print

--- process.js
+++ process.js
@@ -224,7 +224,7 @@ function formatXml(xml) {
     var reg = /(>)(<)(\/*)/g;
     var wsexp = / *(.*) +\n/g;
     var contexp = /(<.+>)(.+\n)/g;
-    xml = xml.replace(reg, '$1\n$2$3').replace(wsexp, '$1\n').replace(contexp, '$1\n$2');
+    xml = xml.replace(reg, '$1\n$2$3').replace(wsexp, '$1\n').replace(contexp, '$1$2');
     var pad = 0;
     var formatted = '';
     var lines = xml.split('\n');
